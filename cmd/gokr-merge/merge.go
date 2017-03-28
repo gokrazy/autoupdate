502eacf00bac90f20f58d984bc52fa7b4df1c2b0
Michael Stapelberg <michael@stapelberg.de>
2017-03-28 09:54:54 +0200

    merge: check for label

--- cmd/gokr-merge/merge.go
+++ cmd/gokr-merge/merge.go
@@ -2,9 +2,15 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"log"
+	"net/http"
 	"os"
+	"strconv"
+	"strings"
+
+	"github.com/google/go-github/github"
 )
 
 var (
@@ -12,6 +18,19 @@ var (
 		"",
 		"name of the required label before the PR will be merged")
 )
+
+func ensureLabel(ctx context.Context, client *github.Client, owner, repo string, issueNum int, label string) (bool, error) {
+	labels, _, err := client.Issues.ListLabelsByIssue(ctx, owner, repo, issueNum, nil)
+	if err != nil {
+		return true, err
+	}
+	for _, l := range labels {
+		if *l.Name == label {
+			return true, nil
+		}
+	}
+	return false, nil
+}
 
 func main() {
 	flag.Parse()
@@ -22,6 +41,7 @@ func main() {
 	}
 
 	for _, name := range []string{
+		"TRAVIS_PULL_REQUEST",
 		"TRAVIS_PULL_REQUEST_BRANCH",
 		"TRAVIS_REPO_SLUG",
 		"GITHUB_USER",
@@ -32,7 +52,36 @@ func main() {
 		}
 	}
 
-	log.Printf("TODO: implement")
+	slug := os.Getenv("TRAVIS_REPO_SLUG")
 
-	os.Exit(2) // label not present
+	parts := strings.Split(slug, "/")
+	if got, want := len(parts), 2; got != want {
+		log.Fatalf("unexpected number of /-separated parts in %q: got %d, want %d", slug, got, want)
+	}
+
+	ctx := context.Background()
+
+	client := github.NewClient(&http.Client{
+		Transport: &github.BasicAuthTransport{
+			Username: os.Getenv("GITHUB_USER"),
+			Password: os.Getenv("GITHUB_AUTH_TOKEN"),
+		},
+	})
+
+	issueNum, err := strconv.ParseInt(os.Getenv("TRAVIS_PULL_REQUEST"), 0, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	found, err := ensureLabel(ctx, client, parts[0], parts[1], int(issueNum), *requireLabel)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if !found {
+		os.Exit(2) // label not present
+	}
+
+	log.Printf("TODO: implement")
+	// TODO: merge the PR
+	// TODO: delete the ref to clean up old branches
 }
